8f0c39fac7c004d1dbcfaf2b3aeb4f55a9e35b21
whc <1532106001@qq.com>
2019-05-06 13:27:17 +0800

    '完成'

--- src/Ebook.vue
+++ src/Ebook.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ebook">
-    <title-bar :isShow="isShow"></title-bar>
+    <title-bar :ifTitleAndMenuShow="ifTitleAndMenuShow"></title-bar>
     <div class="read-wrapper">
       <div id="read"></div>
       <div class="mask">
@@ -9,8 +9,18 @@
         <div class="right" @click="nextPage"></div>
       </div>
     </div>
-    <menu-bar :isShow="isShow" :fontSizeList="fontSizeList" :defalutFontSize ="defalutFontSize" @setFontSize="setFontSize" ref="MenuBar"></menu-bar>
-  </div>
+ <menu-bar :ifTitleAndMenuShow="ifTitleAndMenuShow"
+              :fontSizeList="fontSizeList"
+              :defaultFontSize="defaultFontSize"
+              @setFontSize="setFontSize"
+              :themeList="themeList"
+              :defaultTheme="defaultTheme"
+              @setTheme="setTheme"
+              :bookAvailable="bookAvailable"
+              @onProgressChange="onProgressChange"
+              :navigation="navigation"
+              @jumpTo="jumpTo"
+              ref="menuBar"></menu-bar>  </div>
 </template>
 
 <script>
@@ -24,77 +34,161 @@ export default {
      TitleBar,
      MenuBar
     },
-    data(){
-        return{
-        isShow:false,
-        fontSizeList:[
-          {fontSize:12},
-          {fontSize:14},
-          {fontSize:16},
-          {fontSize:18},
-          {fontSize:20},
-          {fontSize:22},
-          {fontSize:24}
-        ],defalutFontSize:12
+    data() {
+    return {
+      ifTitleAndMenuShow: false,
+      fontSizeList: [
+        { fontSize: 12 },
+        { fontSize: 14 },
+        { fontSize: 16 },
+        { fontSize: 18 },
+        { fontSize: 20 },
+        { fontSize: 22 },
+        { fontSize: 24 }
+      ],
+      defaultFontSize: 16,
+      themeList: [
+        {
+          name: 'default',
+          style: {
+            body: {
+              'color': '#000', 'background': '#fff'
+            }
+          }
+        },
+        {
+          name: 'eye',
+          style: {
+            body: {
+              'color': '#000', 'background': '#ceeaba'
+            }
+          }
+        },
+        {
+          name: 'night',
+          style: {
+            body: {
+              'color': '#fff', 'background': '#000'
+            }
+          }
+        },
+        {
+          name: 'gold',
+          style: {
+            body: {
+              'color': '#000', 'background': 'rgb(241, 236, 226)'
+            }
+          }
         }
-    },
+      ],
+      defaultTheme: 0,
+      // 图书是否处于可用状态
+      bookAvailable: false,
+      navigation: {}
+    }
+  },
   methods: {
-    //电子书的解析和渲染
-    showEpub() {
-      //生成Book对象
-      this.book = new Epub(DOWNLOAD_URL);
-      //生成Rendition对象,通过Book.renderTo生成
-      this.rendition = this.book.renderTo("read", {
-        width: window.innerWidth,
-        height: window.innerHeight
-      });
-      //通过Rendition.display渲染电子书
-      this.rendition.display();
-      this.themes = this.rendition.themes;
-      this.themes.fontSize(this.defalutFontSize)
+    // 根据链接跳转到指定位置
+    jumpTo(href) {
+      this.rendition.display(href)
+      this.hideTitleAndMenu()
     },
+    hideTitleAndMenu() {
+      // 隐藏标题栏和菜单栏
+      this.ifTitleAndMenuShow = false
+      // 隐藏菜单栏弹出的设置栏
+      this.$refs.menuBar.hideSetting()
+      // 隐藏目录
+      this.$refs.menuBar.hideContent()
+    },
+    // progress 进度条的数值（0-100）
+    onProgressChange(progress) {
+      const percentage = progress / 100
+      const location = percentage > 0 ? this.locations.cfiFromPercentage(percentage) : 0
+      this.rendition.display(location)
+    },
+    // 设置主题
+    setTheme(index) {
+      this.themes.select(this.themeList[index].name)
+      this.defaultTheme = index
+    },
+    // 注册主题
+    registerTheme() {
+      this.themeList.forEach(theme => {
+        this.themes.register(theme.name, theme.style)
+      })
+    },
+    // 设置字号大小
+    setFontSize(fontSize) {
+      this.defaultFontSize = fontSize
+      if (this.themes) {
+        this.themes.fontSize(fontSize + 'px')
+      }
+    },
+    // 切换标题和菜单的显示状态
+    toggleTitleAndMenu() {
+      this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow
+      if (!this.ifTitleAndMenuShow) {
+        this.$refs.menuBar.hideSetting()
+      }
+    },
+    // 上一页
     prevPage() {
-      console.log(this.rendition);
-      if(this.isShow){
-          this.isShow = false;
-          return;
-      }
       if (this.rendition) {
-        this.rendition.prev();
+        this.rendition.prev()
       }
     },
+    // 下一页
     nextPage() {
-      console.log(this.rendition);
-      if(this.isShow){
-          this.isShow = false;
-          return;
-      }
       if (this.rendition) {
-        this.rendition.next();
+        this.rendition.next()
       }
-    },toggleTitleAndMenu(){
-        this.isShow = !this.isShow;
-        if(!this.isShow){
-           this.$refs.MenuBar.hideSettingFontSize();
-        }
-    },setFontSize(fontSize){
-      this.defalutFontSize = fontSize;
-      if(this.themes){
-      this.themes.fontSize(fontSize+"px")
-      }
+    },
+    // 电子书的解析和渲染
+    showEpub() {
+      // 生成Book对象
+      this.book = new Epub(DOWNLOAD_URL)
+      // 通过Book.renderTo生成Rendition对象
+      this.rendition = this.book.renderTo('read', {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        // 兼容iOS
+        method: 'default'
+      })
+      // 通过Rendtion.display渲染电子书
+      this.rendition.display()
+      // 获取Theme对象
+      this.themes = this.rendition.themes
+      // 设置默认字体
+      this.setFontSize(this.defaultFontSize)
+      // 注册主题
+      this.registerTheme()
+      // 设置默认主题
+      this.setTheme(this.defaultTheme)
+      // Book对象的钩子函数ready
+      this.book.ready.then(() => {
+        this.navigation = this.book.navigation
+        // 生成Locations对象
+        return this.book.locations.generate()
+      }).then(result => {
+        // 保存locations对象
+        this.locations = this.book.locations
+        // 标记电子书为解析完毕状态
+        this.bookAvailable = true
+      })
     }
   },
   mounted() {
-    this.showEpub();
+    this.showEpub()
   }
-};
+}
 </script>
 
 <style lang='scss' scoped>
-@import "assets/styles/global";
+@import 'assets/styles/global';
 .ebook {
   position: relative;
- .read-wrapper {
+  .read-wrapper {
     .mask {
       position: absolute;
       top: 0;
@@ -116,3 +210,4 @@ export default {
   }
 }
 </style>
+© 2019 GitHub, Inc.
